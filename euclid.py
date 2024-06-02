e7fb4eb22a46883ef171a497a43c3f4d5122ffd3
LeoMassy <r.masahato.24.4.1390@4nonome.com>
2024-06-02 16:34:27 +0900

    制御構文

--- euclid.py
+++ euclid.py
@@ -2,3 +2,40 @@ a = input("a の値を入力: ")
 b = input("b の値を入力: ")
 
 # TODO
+a = 10
+b = 20
+r = a % b
+while(r != 0):
+    a = b
+    b = r
+    r = a % b
+else:
+    print('最大公約数は{}'.format(b))
+
+
+# In[2]:
+
+
+a = 14
+b = 91
+r = a % b
+while(r != 0):
+    a = b
+    b = r
+    r = a % b
+else:
+    print('最大公約数は{}'.format(b))
+
+
+# In[3]:
+
+
+a = 91
+b = 14
+r = a % b
+while(r != 0):
+    a = b
+    b = r
+    r = a % b
+else:
+    print('最大公約数は{}'.format(b))
