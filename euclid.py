f7093e530a40b8c387c7ad906e775b18a6d5f680
LeoMassy <r.masahato.24.4.1390@4nonome.com>
2024-06-03 12:07:14 +0900

    制御構文解答

--- euclid.py
+++ euclid.py
@@ -3,8 +3,9 @@ b = int(input("b の値を入力: "))
 r = a % b
 while(r != 0):
    a,b = b,a%b
+
 else:
-    print('最大公約数は{}'.format(b))
+        print('最大公約数は{}'.format(b))
 
 
 
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -7,4 +7,5 @@ h = (b - a) / n
 x = 0
 for i in range(n):
     x += h * (sin(a + h * i) + sin(a + h * (i + 1))) / 2
+
 print(x)
