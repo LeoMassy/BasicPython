6e217ca7aa0259161e8b19347a8620cbb3956e2f
LeoMassy <r.masahato.24.4.1390@4nonome.com>
2024-06-04 17:39:49 +0900

    python入門の解答直し③

--- pi.py
+++ pi.py
@@ -1,5 +1,5 @@
 
-text = """" How I want a drink, alcoholic of course, after the heavy chapters involving  
+text = """ How I want a drink, alcoholic of course, after the heavy chapters involving  
 quantum mechanics. All of thy geometry, Herr Planck, is fairly hard."""
 
 words = text.split()
