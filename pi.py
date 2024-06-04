7bc29950351cf38a91ead6444ccb2851014b7b38
LeoMassy <r.masahato.24.4.1390@4nonome.com>
2024-06-04 12:39:20 +0900

    python入門の解答直し

--- pi.py
+++ pi.py
@@ -2,7 +2,11 @@
 text = """" How I want a drink, alcoholic of course, after the heavy chapters involving  
 quantum mechanics. All of thy geometry, Herr Planck, is fairly hard."""
 
+words = text.split()
+num = ""
+for i in range(len(words)):
+    num += str(len(words[i]))
 
-"".join(list(map(str,list(map(len, ["How","I", "want","a","drink","alcoholic","of", "course","after", "the", "heavy","chapters", "involving", "quantum", "mechanics","All", "of", "the", "geometry", "Herr", "Planck", "is", "fairly","hard"])))))
+print(num)
 
 
